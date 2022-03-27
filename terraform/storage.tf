5368dd9bd75a77567801363bef254c31f9fcd05b
Lukas Karlsson <admin@lukwam.dev>
2022-03-27 01:08:17 +0000

    Updated terraform config.

---
+++ terraform/storage.tf
@@ -0,0 +1,23 @@
+resource "google_storage_bucket" "answers" {
+  name          = "lukwam-hex-answers"
+  project       = google_project.project.project_id
+  location      = "us-east4"
+  force_destroy = false
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "puzzles" {
+  name          = "lukwam-hex-puzzles"
+  project       = google_project.project.project_id
+  location      = "us-east4"
+  force_destroy = false
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "thumbnails" {
+  name          = "lukwam-hex-thumbnails"
+  project       = google_project.project.project_id
+  location      = "us-east4"
+  force_destroy = false
+  uniform_bucket_level_access = true
+}
