65a6dac57b90da91f186d896b1af728852220e26
Lukas Karlsson <admin@lukwam.dev>
2022-04-02 02:41:52 +0000

    Downoad file from gcs and then remove temp file.

--- images/pdf2png/main.py
+++ images/pdf2png/main.py
@@ -4,6 +4,7 @@ import os
 
 from flask import Flask
 from flask import request
+from google.cloud import storage
 
 app = Flask(__name__)
 
@@ -13,11 +14,25 @@ def index():
     event = request.get_json()
 
     bucket_name = event["bucket"]
-    content_type = event["contentType"]
     file_name = event["name"]
 
+    # content_type = event["contentType"]
     path = f"gs://{bucket_name}/{file_name}"
-    print(f"Path: {path} [{content_type}]")
+    # print(f"Path: {path} [{content_type}]")
+
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blob = storage.Blob(file_name, bucket)
+
+    tmp_file = f"/tmp/{file_name}"
+
+    # download file
+    print(f"Downloading file {path}...")
+    blob.download_to_filename(tmp_file)
+
+    # remove downloaded file
+    os.remove(tmp_file)
+    print(f"Removed file {tmp_file}.")
 
     return "ok"
 
