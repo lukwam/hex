c500c79ec529b4a311058fb50f4ff5092161ee4b
Lukas Karlsson <admin@lukwam.dev>
2022-04-02 02:15:55 +0000

    Print out path and content type.

--- images/pdf2png/main.py
+++ images/pdf2png/main.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """PDF to PNG."""
-import base64
-import json
 import os
 
 from flask import Flask
@@ -12,56 +10,16 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def index():
-    envelope = request.get_json()
-    if not envelope:
-        msg = "no Pub/Sub message received"
-        print(f"error: {msg}")
-        return f"Bad Request: {msg}", 400
+    event = request.get_json()
 
-    if not isinstance(envelope, dict) or "message" not in envelope:
-        msg = "invalid Pub/Sub message format"
-        print(f"error: {msg}")
-        print(envelope)
-        return f"Bad Request: {msg}", 400
+    bucket_name = event["bucket"]
+    content_type = event["contentType"]
+    file_name = event["name"]
 
-    # Decode the Pub/Sub message.
-    pubsub_message = envelope["message"]
+    path = f"g://{bucket_name}/{file_name}"
+    print(f"Path: {path} [{content_type}")
 
-    if isinstance(pubsub_message, dict) and "data" in pubsub_message:
-        try:
-            data = json.loads(
-                base64.b64decode(
-                    pubsub_message["data"],
-                ).decode(),
-            )
-
-        except Exception as e:
-            msg = (
-                "Invalid Pub/Sub message: "
-                "data property is not valid base64 encoded JSON"
-            )
-            print(f"error: {e}")
-            return f"Bad Request: {msg}", 400
-
-        # Validate the message is a Cloud Storage event.
-        if not data["name"] or not data["bucket"]:
-            msg = (
-                "Invalid Cloud Storage notification: "
-                "expected name and bucket properties"
-            )
-            print(f"error: {msg}")
-            return f"Bad Request: {msg}", 400
-
-        try:
-            print("Hello World.")
-            print(data)
-            return ("", 204)
-
-        except Exception as e:
-            print(f"error: {e}")
-            return ("", 500)
-
-    return ("", 500)
+    return "ok"
 
 
 if __name__ == "__main__":
