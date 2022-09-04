1843cfe997e89d9bdb23df4d0565805f835f1287
Lukas Karlsson <admin@lukwam.dev>
2022-09-04 04:37:25 +0000

    Added support for archive.

--- terraform/eventarc.tf
+++ terraform/eventarc.tf
@@ -15,6 +15,27 @@ resource "google_eventarc_trigger" "answers-to-image2png" {
             region  = var.region
         }
     }
+    service_account = "${google_project.project.project_id}@appspot.gserviceaccount.com"
+}
+
+resource "google_eventarc_trigger" "archive-to-image2png" {
+    name            = "archive-to-image2png"
+    location        = var.region
+    matching_criteria {
+        attribute   = "type"
+        value       = "google.cloud.storage.object.v1.finalized"
+    }
+    matching_criteria {
+        attribute   = "bucket"
+        value       = google_storage_bucket.archive.name
+    }
+    destination {
+        cloud_run_service {
+            service = "image2png"
+            region  = var.region
+        }
+    }
+    service_account = "${google_project.project.project_id}@appspot.gserviceaccount.com"
 }
 
 resource "google_eventarc_trigger" "puzzles-to-image2png" {
@@ -34,4 +55,5 @@ resource "google_eventarc_trigger" "puzzles-to-image2png" {
             region  = var.region
         }
     }
+    service_account = "${google_project.project.project_id}@appspot.gserviceaccount.com"
 }
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -14,6 +14,14 @@ resource "google_storage_bucket" "archive" {
   uniform_bucket_level_access = true
 }
 
+resource "google_storage_bucket" "archive-images" {
+  name          = "lukwam-hex-archive-images"
+  project       = google_project.project.project_id
+  location      = "us-east4"
+  force_destroy = false
+  uniform_bucket_level_access = true
+}
+
 resource "google_storage_bucket" "guide" {
   name          = "lukwam-hex-guide"
   project       = google_project.project.project_id
@@ -52,6 +60,12 @@ resource "google_storage_bucket" "wordpress" {
   location      = "us-east4"
   force_destroy = false
   uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "archive-images-image-reader" {
+  bucket = google_storage_bucket.archive-images.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.image-reader.email}"
 }
 
 resource "google_storage_bucket_iam_member" "images-image-reader" {
