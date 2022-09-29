6bd954d176ffd859f7f8a05eae8bcd80f735599d
Lukas Karlsson <admin@lukwam.dev>
2022-09-29 22:02:41 +0000

    Disabled the cloud sql instance.

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -1,21 +1,22 @@
-resource "google_sql_database_instance" "mysql01" {
-  name             = "mysql01"
-  database_version = "MYSQL_8_0"
-  region           = "us-east4"
+# resource "google_sql_database_instance" "mysql01" {
+#   name             = "mysql01"
+#   database_version = "MYSQL_8_0"
+#   region           = "us-east4"
 
-  settings {
-    tier = "db-f1-micro"
-  }
-}
+#   settings {
+#     tier = "db-f1-micro"
+#   }
+#   deletion_protection = false
+# }
 
-resource "google_sql_database" "wordpress" {
-  name     = "wordpress"
-  instance = google_sql_database_instance.mysql01.name
-}
+# resource "google_sql_database" "wordpress" {
+#   name     = "wordpress"
+#   instance = google_sql_database_instance.mysql01.name
+# }
 
-resource "google_sql_user" "wordpress" {
-  name     = "wordpress"
-  instance = google_sql_database_instance.mysql01.name
-  host     = "%"
-  password = "wordpress"
-}
+# resource "google_sql_user" "wordpress" {
+#   name     = "wordpress"
+#   instance = google_sql_database_instance.mysql01.name
+#   host     = "%"
+#   password = "wordpress"
+# }
