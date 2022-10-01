cde3447949db411e3e803c1d9744a568272fe2bb
Lukas Karlsson <admin@lukwam.dev>
2022-10-01 21:34:12 +0000

    Added filters.

--- docs/script.js
+++ docs/script.js
@@ -6,15 +6,18 @@ var puzzles = []
 
 var book_codes = {}
 var publication_codes = {}
-// var puzzle_stats = {}
+var puzzle_ids = {}
 
 function displayBooks() {
     var books_list = document.getElementById("books-list");
     books_list.innerHTML = "";
     for (n=0; n<books.length; n++) {
         book = books[n];
+        var title = '<a id="' + book.id + '" onclick="filterPuzzlesByBook(this);">' + book.title + '</a>';
         var li = document.createElement("li");
-        li.innerHTML = book.title;
+        li.innerHTML = title;
+        li.setAttribute("data-id", book.id);
+        li.setAttribute("data-code", book.code);
         books_list.appendChild(li);
     }
 }
@@ -24,8 +27,11 @@ function displayPublications() {
     publications_list.innerHTML = "";
     for (n=0; n<publications.length; n++) {
         publication = publications[n];
+        var name = '<a id="' + publication.id + '" onclick="filterPuzzlesByPub(this);">' + publication.name + '</a>';
         var li = document.createElement("li");
-        li.innerHTML = publication.name + " (" + publication.code + ")";
+        li.innerHTML = name + " (" + publication.code + ")";
+        li.setAttribute("data-id", publication.id);
+        li.setAttribute("data-pub", publication.code)
         publications_list.appendChild(li);
     }
 }
@@ -35,10 +41,53 @@ function displayPuzzles() {
     puzzles_list.innerHTML = "";
     for (n=0; n<puzzles.length; n++) {
         puzzle = puzzles[n];
+        var title = puzzle.title;
+        if (puzzle.puzzle_link) {
+            title = '<a href="' + puzzle.puzzle_link + '" target="_puzzle">' + puzzle.title + '</a>';
+        } else if (puzzle.web_link) {
+            title = '<a href="' + puzzle.web_link + '" target="_puzzle">' + puzzle.title + '</a>';
+        }
+        var answer = "";
+        if (puzzle.answer_link) {
+            answer = ' | <a href="' + puzzle.answer_link + '" target="_puzzle">answer</a>';
+        }
         var li = document.createElement("li");
-        li.innerHTML = formatDate(puzzle.date) + " " + puzzle.title + " (" + puzzle.pub + ")";
+        li.id = "puzzle-" + puzzle.id;
+        li.setAttribute("data-id", puzzle.id);
+        li.setAttribute("data-pub", puzzle.pub);
+        li.innerHTML = formatDate(puzzle.date) + " " + title + answer + " (" + puzzle.pub + ")";
         puzzles_list.appendChild(li);
     }
+}
+
+// filter puzzles by book
+function filterPuzzlesByBook(book_link) {
+    var code = book_link.parentElement.dataset.code;
+    var puzzle_list = document.getElementById("puzzles-list");
+    [...puzzle_list.children].forEach(element => {
+        var puzzle_id = element.dataset.id;
+        var puzzle = puzzle_ids[puzzle_id];
+        if (puzzle.books && puzzle.books.includes(code)) {
+            element.style.display = "list-item";
+        } else {
+            element.style.display = "none";
+        }
+        element.date = new Date(Date.parse(element.date));
+    });
+}
+
+// filter puzzles by publication
+function filterPuzzlesByPub(pub_link) {
+    var pub = pub_link.parentElement.dataset.pub;
+    var puzzle_list = document.getElementById("puzzles-list");
+    [...puzzle_list.children].forEach(element => {
+        if (element.dataset.pub == pub) {
+            element.style.display = "list-item";
+        } else {
+            element.style.display = "none";
+        }
+        element.date = new Date(Date.parse(element.date));
+    });
 }
 
 // format dates to yyyy-mm-dd format
@@ -105,20 +154,40 @@ function getPuzzles() {
     return puzzles
 }
 
+// update puzzles_ids dict
+function getPuzzlesById() {
+    for (n=0; n<puzzles.length; n++) {
+        var puzzle = puzzles[n];
+        var id = puzzle["id"];
+        puzzle_ids[id] = puzzle;
+    }
+}
+
 // load data from the API
 function loadData() {
     books = getBooks();
     getBooksByCode();
+    displayBooks();
     console.log("Books: " + books.length);
 
     publications = getPublications();
     getPublicationsByCode();
+    displayPublications();
     console.log("Publications: " + publications.length);
 
     puzzles = getPuzzles();
+    getPuzzlesById();
+    displayPuzzles();
     console.log("Puzzles: " + puzzles.length);
 
-    displayBooks();
-    displayPublications();
-    displayPuzzles();
+
+
+}
+
+// show all puzzles
+function showAllPuzzles() {
+    var puzzle_list = document.getElementById("puzzles-list");
+    [...puzzle_list.children].forEach(element => {
+        element.style.display = "list-item";
+    });
 }
